eed40b2cbde7afbbed263513945de59dc186d501
ryuichi-isoda <ryuichi.isoda1007@gmail.com>
2019-09-20 09:15:46 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,6 +6,7 @@ module UsersHelper
   #   image_tag(gravatar_url, alt: user.name, class: "gravatar")
   # end
 
+  # 渡されたユーザーのGravatar画像を返す
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
